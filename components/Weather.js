1c123f2b70adbf3e3d3e446cc4e77ffb101bf510
Kadir <92851621+KadirAkyr@users.noreply.github.com>
2022-11-04 16:11:30 +0100

    ✨ Add forecasts at home screen

---
+++ components/Weather.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { Text, View, StyleSheet, Image, ScrollView } from "react-native";
+
+export default function weather({ forecast }) {
+  const getIcon = (icon) => `http://openweathermap.org/img/wn/${icon}@2x.png`;
+  return (
+    <View style={styles.container}>
+      <Text> {forecast.hour}h </Text>
+      <Image
+        source={{
+          uri: getIcon(forecast.icon),
+        }}
+        style={styles.img}
+      />
+      <Text style={styles.temp}> {forecast.temp} °C </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#E8AA42",
+    height: 140,
+    width: 70,
+    paddingVertical: 6,
+    justifyContent: "center",
+    alignItems: "center",
+    marginRight: 10,
+    borderRadius: 50,
+  },
+  img: {
+    width: 50,
+    height: 50,
+  },
+  temp: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
